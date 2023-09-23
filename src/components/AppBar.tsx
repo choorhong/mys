1b01b5890dfb08da77a9c2faff0c8dda392ebd0c
Choor Hong <abner.hong@hotmail.com>
2023-09-23 12:31:16 +0800

    Update app icon

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,6 +1,6 @@
 import { AppBar, Container, Toolbar, Typography } from "@mui/material";
 
-import AdbIcon from "@mui/icons-material/Adb";
+import ShowChatIcon from "@mui/icons-material/ShowChart";
 import { useNavigate } from "react-router-dom";
 
 const ResponsiveAppBar = () => {
@@ -9,7 +9,7 @@ const ResponsiveAppBar = () => {
     <AppBar position="static" sx={{ mb: 2 }} className="app-bar">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { md: "flex" }, mr: 1 }} />
+          <ShowChatIcon sx={{ display: { md: "flex" }, mr: 1 }} />
           <Typography
             variant="h6"
             noWrap
